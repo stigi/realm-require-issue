5a0b8fdfaa747e4fe9b021b9a93e1030402fe26f
Ullrich Schäfer <git@ullrich.is>
2018-01-11 14:04:38 +0100

    Adding test to demonstrate require('realm') issue

---
+++ index.test.js
@@ -0,0 +1,9 @@
+// yarn jest index.test.js
+// uncomment this line and jest won't exit after the test passed
+//require('realm')
+
+describe("a test", function() {
+  test("testing", function(){
+
+  })
+})
